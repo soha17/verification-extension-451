ace23a99426f2f5f53f2a9d27fdbfaef8e139d73
Soha Jiwani <28872527+soha17@users.noreply.github.com>
2022-03-27 13:55:31 -0400

    added in app.py

---
+++ verification_microservices/app.py
@@ -0,0 +1,40 @@
+# Citing https://code.visualstudio.com/docs/python/tutorial-flask as big help for this 
+# add in john patel
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Flask is running"
+
+@app.route("/initial_verification/<first_name>/<last_name>/<id_number>")
+def initial_verification(first_name, last_name, id_number):
+    # below line is fake CA database - later versions will have to use a real database
+    userDB = ['{ "first_name":"John", "last_name":"Wick", "id_number":"123456789-005" }']
+    # print(first_name + last_name + id_number)
+    for i in userDB:
+        curr = json.loads(i)
+        # print(curr["id_number"])
+        # print(id_number)
+        if id_number == curr["id_number"]:
+            # print("in if")
+            if first_name == curr["first_name"] and last_name == curr["last_name"]:
+                return "1"
+            else:
+                return "0"
+    return "-1"
+
+@app.route("/current-verification-status/<username>/")
+def current_verification(username):
+    # userdb in this case represents our own internal user DB - assuming account 
+    userDB = ['{ "first_name":"John", "last_name":"Wick", "username":"jwick", "id_number":"123456789-005", "verified":"1", "valid":"0" }']
+    for i in userDB:
+        curr = json.loads(i)
+        # print(curr["id_number"])
+        # print(id_number)
+        if username == curr["username"]:
+            # print("in if")
+            return curr["valid"]
+    return "-1"
