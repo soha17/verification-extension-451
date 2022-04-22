01851109897856331ec8ed906a7481c1cc2399bf
Navid <46184137+NavidK27@users.noreply.github.com>
2022-04-22 10:26:56 -0400

    Add files via upload

--- verifyButton.js
+++ verifyButton.js
@@ -5,6 +5,34 @@ $(document).ready(function () {
           //make call to verification service to see if user is verified
           //if user is verified, check the form element for uni-verify to 1
           //let verificationSiganl = "0"; // api call and store the response in this.
+
+
+
+
+          var otp_veri = document.getElementById("otp").value;
+          if(otp_veri==""){
+            var url = "https://api.twilio.com/2010-04-01/Accounts/ACabf6358c3e7fd3c8d33b856fe467bd5f/Messages";
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url);
+
+            xhr.setRequestHeader("Accept", "application/json");
+            xhr.setRequestHeader("Authorization", "Basic " + btoa("ACabf6358c3e7fd3c8d33b856fe467bd5f:9ac66c87cd6fd7794a9779d364c427b8"));
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            xhr.send("Body=Welcome to Uni-Verify! Your OTP is 451154&To=+14127421854&From=+19377447376");
+            xhr.onreadystatechange = function () {
+                 if (xhr.readyState === 4) {
+                   console.log(xhr.status);
+                   console.log(xhr.responseText);
+                 }};
+          }
+          else if(otp_veri!="451154"){
+            alert("Incorrect OTP");
+            return false;
+          }
+          else if(otp_veri=="451154"){
+
+
           let verificationSiganl = $.get("http://127.0.0.1:5000/current-verification-status/jwick/", function(verificationStatus){
               verificationSiganl = JSON.stringify(verificationStatus);
               // alert(verificationSiganl);
@@ -17,5 +45,6 @@ $(document).ready(function () {
               console.log(response.farewell);
             });
           });
+        }
        });
  });
